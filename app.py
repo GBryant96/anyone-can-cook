a8e9f965a6f127c2afdf42420e92937f4ba818d8
GBryant96 <gizmo_george@hotmail.com>
2021-04-05 19:10:00 +0000

    pymongo error - bad authentication

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
+@app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("cookbook.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
