cd3420a6b664bb513a9e9fdea93f82cf5ecd39e4
GBryant96 <gizmo_george@hotmail.com>
2021-04-05 16:03:25 +0000

    testing heroku

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/get_tasks")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=recipes)
+    return render_template("cookbook.html", recipes=recipes)
 
 
 if __name__ == "__main__":
