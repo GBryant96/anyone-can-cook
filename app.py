21289b99a153b6ec2eca33adc3a2aca3fc87b8dd
GBryant96 <gizmo_george@hotmail.com>
2021-04-07 15:25:10 +0000

    create add_recipe template

--- app.py
+++ app.py
@@ -97,6 +97,10 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
    
    
 if __name__ == "__main__":
